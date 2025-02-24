2765ca355bb28580d1199a5a6f465d7efc08e187
HafidElmoudden <abdulhafid858@gmail.com>
2025-02-24 10:25:35 +0000

    feat: Add constructor-based dependency injection for MetierImpl

--- src/main/java/metier/MetierImpl.java
+++ src/main/java/metier/MetierImpl.java
@@ -5,6 +5,15 @@ import dao.IDao;
 public class MetierImpl implements IMetier {
     private IDao dao;
 
+    public MetierImpl() {
+        System.out.println("Constructeur MetierImpl sans paramètre");
+    }
+
+    public MetierImpl(IDao dao) {
+        this.dao = dao;
+        System.out.println("Constructeur MetierImpl avec paramètre");
+    }
+
     @Override
     public double calcul() {
         double t = dao.getData();
