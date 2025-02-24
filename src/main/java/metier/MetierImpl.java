95ef0988f6999160243e2e673ca9ae9457ed1a93
HafidElmoudden <abdulhafid858@gmail.com>
2025-02-24 10:02:36 +0000

    feat: Metier interface and a Metier implementation

---
+++ src/main/java/metier/MetierImpl.java
@@ -0,0 +1,19 @@
+package metier;
+
+import dao.IDao;
+
+public class MetierImpl implements IMetier {
+    private IDao dao;
+
+    @Override
+    public double calcul() {
+        double t = dao.getData();
+        double res = t * 23;
+
+        return res;
+    }
+
+    public void setDao(IDao dao) {
+        this.dao = dao;
+    }
+}
